79cfae8cd37133c00f0c6a2853acdc036c008db7
SashaSamoylov <dragonboy950501@outlook.com>
2022-04-17 11:02:33 +0300

    orders mobile

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,44 +1,44 @@
 <template>
   <div class="details py-12">
-    <div class="details-card card pt-8 pb-32 px-10">
-      <div class="card-header flex mb-5">
+    <div class="details-card card">
+      <div class="card-header flex">
         <h4 class="text-white">BTC/USDT 43591.13</h4>
-        <p class="text-green ml-8">43609.27 +0.12%</p>
+        <p class="text-green">43609.27 +0.12%</p>
       </div>
       <div class="card-body">
-        <div class="border-b border-black pb-5">
-          <p>浮动盈亏 USDT</p>
+        <div class="card-body__item border-b border-black pb-5">
+          <p>Profit/Loss USDT</p>
           <p class="text-green text-big">+0.06 <span>/-0.1%</span></p>
-          <p>止盈 +300 价格 48020.10 / 止播 -48 价格 42902.44</p>
+          <p>Stop Profit +300 Price 48020.10 / Stop Lost - 48 Price 42902.44</p>
         </div>
         <div class="flex pt-5">
           <div>
-            <p>买入市值 USDT</p>
+            <p>Option Price USDT</p>
             <div class="text-white text-big">2965<span>.37</span></div>
-            <p>BTC 0.068 个</p>
+            <p>BTC 0.068 pcs</p>
           </div>
-          <div class="ml-20">
-            <p>保证金</p>
+          <div class="m:ml-20 ml-auto">
+            <p>Deposit</p>
             <div class="text-white text-big">60<span>.00</span></div>
-            <p>杠杆 =49x</p>
+            <p>Multiply =49x</p>
           </div>
         </div>
       </div>
       <div class="card-footer mt-14">
         <div class="card-footer__list flex py-3 border-b border-black" @click="isModal = true">
-          <div class="list-name"><span class="text-green">买涨</span>价格</div>
+          <div class="list-name"><span class="text-green">Buy </span>Price</div>
           <div class="list-value ml-auto text-white">43608.33</div>
         </div>
         <div class="card-footer__list flex py-3 border-b border-black">
-          <div class="list-name"><span class="text-green">买涨</span>时间</div>
+          <div class="list-name"><span class="text-green">Buy </span>Time</div>
           <div class="list-value ml-auto">2022-01-13 13:20:53</div>
         </div>
         <div class="card-footer__list flex py-3 border-b border-black">
-          <div class="list-name">平仓价格</div>
+          <div class="list-name">Close Price</div>
           <div class="list-value ml-auto">--</div>
         </div>
         <div class="card-footer__list flex py-3">
-          <div class="list-name">平仓时间</div>
+          <div class="list-name">Close Time</div>
           <div class="list-value ml-auto">--</div>
         </div>
       </div>
